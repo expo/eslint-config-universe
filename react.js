80f42bae74c719b47b3722e51f64c27faae03320
Adam Miskiewicz <adam@sk3vy.com>
2016-06-06 18:08:01 -0700

    Enable jsx-no-bind rule.

--- react.js
+++ react.js
@@ -36,7 +36,7 @@ module.exports = {
     'react/jsx-indent': [1, 2],
     'react/jsx-key': 0,
     'react/jsx-max-props-per-line': 0,
-    'react/jsx-no-bind': 0,
+    'react/jsx-no-bind': 1,
     'react/jsx-no-duplicate-props': 2,
     'react/jsx-no-literals': 0,
     'react/jsx-no-undef': 2,
